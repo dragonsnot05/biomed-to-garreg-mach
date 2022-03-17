99472faecc44070b99e7bdb08a33c336c457ea62
Aidan Veney <62819140+SeveralCircles@users.noreply.github.com>
2022-03-17 14:32:12 -0400

    die

--- Source/content_script.js
+++ Source/content_script.js
@@ -48,7 +48,7 @@ function handleText(textNode) {
     return "Garreg Mach";
   });
   // Deal with butts
-  v = v.replace(/\bmsastem)/g, function(match, p1, p2, offset, string) {
+  v = v.replace(/\bmsastem/g, function(match, p1, p2, offset, string) {
     // c - 1 = b
     // b = String.fromCharCode(p2.charCodeAt(0) - 1);
     return "garregmach";
